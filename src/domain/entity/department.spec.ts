82fb0b355271f9491bd9ba2a79ca6f173766c4ae
João Matos <joaomarcosm.silva@outlook.com>
2023-05-22 19:51:22 -0300

    fix: add missing props

--- src/domain/entity/department.spec.ts
+++ src/domain/entity/department.spec.ts
@@ -1,12 +1,15 @@
+import { randomInt } from 'node:crypto';
 import { Department } from './department';
 
 describe('Department', () => {
   it('should create a new department', () => {
     const department = Department.create({
+      id: randomInt(1, 1000),
       name: 'IOT',
-      cost_center: 242042,
+      cost_center: 2485353,
       is_board: false,
       board: 'Tecnologia da Informação',
+      responsible_id: 'carlos.alexandre',
     });
 
     expect(department).toBeInstanceOf(Department);
