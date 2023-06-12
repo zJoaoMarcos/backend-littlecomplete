5c0b0d96c8c16441997a2f8df4df94decdb62e17
João Matos <joaomarcosm.silva@outlook.com>
2023-06-12 18:03:38 -0300

    create fetch all items use case

--- src/domain/stock/repository/item.respository.ts
+++ src/domain/stock/repository/item.respository.ts
@@ -1,6 +1,13 @@
+import { PaginationParams } from '@/core/repositories/pagination-params';
 import { Item } from '../entity/item';
 
 export interface IItemRepository {
   create(item: Item): Promise<void>;
+  findMany(params: PaginationParams): Promise<FindManyResponse>;
   findByName(name: string): Promise<Item | null>;
+}
+
+interface FindManyResponse {
+  items: Item;
+  totalCount: number;
 }
---
+++ src/domain/stock/use-cases/fetch-all-items.ts
@@ -0,0 +1,24 @@
+import { PaginationParams } from '@/core/repositories/pagination-params';
+import { Item } from '../entity/item';
+import { IItemRepository } from '../repository/item.respository';
+
+interface FetchAllItemsRequest {
+  params: PaginationParams;
+}
+
+interface FetchAllItemsResponse {
+  items: Item;
+  totalCount: number;
+}
+
+export class FetchAllItemsUseCase {
+  constructor(private itemRepository: IItemRepository) {}
+
+  async execute({
+    params,
+  }: FetchAllItemsRequest): Promise<FetchAllItemsResponse> {
+    const { items, totalCount } = await this.itemRepository.findMany(params);
+
+    return { items, totalCount };
+  }
+}
