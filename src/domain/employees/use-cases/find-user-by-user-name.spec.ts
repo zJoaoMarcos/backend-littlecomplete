921fbeb7bcdd56fcb8a64155990349ebf3043197
João Matos <joaomarcosm.silva@outlook.com>
2023-06-19 11:49:11 -0300

    fix tests and resolve error in paths

--- src/domain/employees/use-cases/find-user-by-user-name.spec.ts
+++ src/domain/employees/use-cases/find-user-by-user-name.spec.ts
@@ -1,56 +1,30 @@
-import { InMemoryDepartmentRepository } from '../../../infra/repository/in-memory/in-memory-department-repository';
+import { InMemoryUserAssignmentsRepository } from '@/infra/repository/in-memory/in-memory-user-assigments-repository';
 import { InMemoryUserRepository } from '../../../infra/repository/in-memory/in-memory-user-repository';
-import { CreateUserUseCase } from './create-user';
+import { makeCreateUser } from './factories/make-create-user';
 import { FindUserByUserNameUseCase } from './find-user-by-user-name';
 
 let usersRepository: InMemoryUserRepository;
-let departmentsRepository: InMemoryDepartmentRepository;
-let createUser: CreateUserUseCase;
+let userAssignmentsRepository: InMemoryUserAssignmentsRepository;
 let sut: FindUserByUserNameUseCase;
 
 describe('Find All Users Use Case', () => {
   beforeEach(() => {
     usersRepository = new InMemoryUserRepository();
-    departmentsRepository = new InMemoryDepartmentRepository();
-    createUser = new CreateUserUseCase(usersRepository, departmentsRepository);
-    sut = new FindUserByUserNameUseCase(usersRepository);
+    userAssignmentsRepository = new InMemoryUserAssignmentsRepository();
+    sut = new FindUserByUserNameUseCase(
+      usersRepository,
+      userAssignmentsRepository,
+    );
   });
 
   it('should be able to find all users', async () => {
-    await departmentsRepository.departments.push({
-      name: 'IOT',
-      cost_center: 2420424,
-      is_board: false,
-      board: 'Tecnologia da Informação',
-    });
+    usersRepository.users.push(
+      makeCreateUser(),
+      makeCreateUser({ user_name: 'jane.doe' }),
+    );
 
-    await createUser.execute({
-      user_name: 'jhon.doe',
-      complete_name: 'Jhon Doe',
-      title: 'CTO',
-      department_id: 'IOT',
-      telephone: null,
-      direct_boss: 'Martin Fowler',
-      smtp: 'jhon.doe@email.com',
-      admission_date: '18/04/2000',
-      demission_date: null,
-      status: 'vacation',
-    });
-    await createUser.execute({
-      user_name: 'jane.doe',
-      complete_name: 'jane Doe',
-      title: 'CTO',
-      department_id: 'IOT',
-      telephone: null,
-      direct_boss: 'Martin Fowler',
-      smtp: 'jane.doe@email.com',
-      admission_date: '18/05/2002',
-      demission_date: null,
-      status: 'active',
-    });
+    const { user } = await sut.execute({ userName: 'jane.doe' });
 
-    const { user } = await sut.execute('jane.doe');
-
-    expect(user.complete_name).toEqual('jane Doe');
+    expect(user.user_name).toEqual('jane.doe');
   });
 });
--- src/domain/employees/use-cases/find-user-by-user-name.ts
+++ src/domain/employees/use-cases/find-user-by-user-name.ts
@@ -1,6 +1,17 @@
 import { IUserRepository } from '@/domain/employees/repository/user.repository';
-import { UserNotFoundError } from '@/domain/errors/user-not-found';
+import { Equipment } from '@/domain/inventory/entity/equipment';
 import { IUserAssignmentsRepository } from '@/domain/inventory/repository/user-assignments.repository';
+import { User } from '../entity/user';
+import { UserNotFoundError } from './errors/user-not-found';
+
+interface FindUserByUserNameRequest {
+  userName: string;
+}
+
+interface FindUserByUserNameResponse {
+  user: User;
+  equipments: Equipment[];
+}
 
 export class FindUserByUserNameUseCase {
   constructor(
@@ -8,7 +19,9 @@ export class FindUserByUserNameUseCase {
     private userAssignmentsRepository: IUserAssignmentsRepository,
   ) {}
 
-  async execute(userName: string): Promise<FindUserByUserNameOutput> {
+  async execute({
+    userName,
+  }: FindUserByUserNameRequest): Promise<FindUserByUserNameResponse> {
     const user = await this.userRepository.findByUserName(userName);
 
     if (!user) {
@@ -25,54 +38,3 @@ export class FindUserByUserNameUseCase {
     };
   }
 }
-
-type FindUserByUserNameOutput = {
-  user: {
-    props: {
-      user_name: string;
-      complete_name: string;
-      title: string;
-      department: { id: number | null; name: string | null };
-      telephone: number | null;
-      direct_boss: string;
-      smtp: string;
-      admission_date: Date | null;
-      demission_date: Date | null;
-      status: string;
-    };
-  };
-  equipments: {
-    props: {
-      id: string;
-      status: string;
-      currentUser: string | null;
-      patrimony: string | null;
-      type: string | null;
-      brand: string | null;
-      model: string | null;
-      serviceTag: string | null;
-      purchase: {
-        invoice: string | null;
-        supplier: string | null;
-        purchaseDate: Date | null;
-        warranty: string | null;
-      };
-      department: {
-        id: number | null;
-        name: string | null;
-      };
-      config: {
-        cpu: string | null;
-        ram: string | null;
-        video: string | null;
-        storage: {
-          slots: number | null;
-          storage0Type: string | null;
-          storage0Syze: number | null;
-          storage1Type: string | null;
-          storage1Syze: number | null;
-        };
-      };
-    };
-  }[];
-};
