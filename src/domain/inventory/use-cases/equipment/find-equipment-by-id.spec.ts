678fc1e3187538eee48eabea9dfe11b239ad2068
João Matos <joaomarcosm.silva@outlook.com>
2023-06-19 11:59:45 -0300

    fix and resolves error files paths, and adpat to new entity prop type

--- src/domain/inventory/use-cases/equipment/find-equipment-by-id.spec.ts
+++ src/domain/inventory/use-cases/equipment/find-equipment-by-id.spec.ts
@@ -1,5 +1,7 @@
-import { InMemoryDepartmentRepository } from '../../../infra/repository/in-memory/in-memory-department-repository';
-import { InMemoryEquipmentRepository } from '../../../infra/repository/in-memory/in-memory-equipment-repository';
+import { InMemoryDepartmentRepository } from '@/infra/repository/in-memory/in-memory-department-repository';
+import { InMemoryEquipmentRepository } from '@/infra/repository/in-memory/in-memory-equipment-repository';
+import { Equipment } from '../../entity/equipment';
+import { makeCreateEquipment } from '../factories/make-create-equipment';
 import { FindEquipmentByIdUseCase } from './find-equipment-by-id';
 
 let equipmentsRepository: InMemoryEquipmentRepository;
@@ -14,36 +16,15 @@ describe('Find All Equipments Use Case', () => {
   });
 
   it('should be able list all Equipments', async () => {
-    await departmentsRepository.departments.push({
-      name: 'IOT',
-      cost_center: 2420424,
-      is_board: false,
-      board: 'Tecnologia da Informação',
-    });
+    equipmentsRepository.equipments.push(
+      makeCreateEquipment(),
+      makeCreateEquipment({ id: 'generic.name', type: 'notebook' }),
+      makeCreateEquipment(),
+    );
 
-    await equipmentsRepository.equipments.push({
-      id: '01-005-00132',
-      brand: 'Dell',
-      model: 'XPTO',
-      department: 'TI',
-      status: 'activate',
-      ram: '16GB',
-      supplier: '',
-      invoice: '',
-      warranty: '',
-      purchase_date: '',
-      cpu: '',
-      slots: 0,
-      storage0_type: '',
-      storage0_syze: 0,
-      storage1_type: '',
-      storage1_syze: 0,
-      video: '',
-      service_tag: '',
-    });
+    const { equipment } = await sut.execute({ id: 'generic.name' });
 
-    await expect(() => {
-      sut.execute('01-005-001234');
-    }).resolves;
+    expect(equipment).toBeInstanceOf(Equipment);
+    expect(equipment.type).toEqual('notebook');
   });
 });
