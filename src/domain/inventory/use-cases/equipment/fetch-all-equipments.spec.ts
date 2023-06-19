4121c0699d87e350576e16bbdfc3ee9a3fbc6be8
João Matos <joaomarcosm.silva@outlook.com>
2023-06-19 11:54:44 -0300

    fix test to new entity type

--- src/domain/inventory/use-cases/equipment/fetch-all-equipments.spec.ts
+++ src/domain/inventory/use-cases/equipment/fetch-all-equipments.spec.ts
@@ -1,70 +1,35 @@
-import { InMemoryDepartmentRepository } from '../../../infra/repository/in-memory/in-memory-department-repository';
-import { InMemoryEquipmentRepository } from '../../../infra/repository/in-memory/in-memory-equipment-repository';
-import { FindAllEquipmentsUseCase } from './find-all-equipments';
+import { InMemoryDepartmentRepository } from '@/infra/repository/in-memory/in-memory-department-repository';
+import { InMemoryEquipmentRepository } from '@/infra/repository/in-memory/in-memory-equipment-repository';
+import { Equipment } from '../../entity/equipment';
+import { makeCreateEquipment } from '../factories/make-create-equipment';
+import { FetchAllEquipmentsUseCase } from './fetch-all-equipments';
 
 let equipmentsRepository: InMemoryEquipmentRepository;
 let departmentsRepository: InMemoryDepartmentRepository;
-let sut: FindAllEquipmentsUseCase;
+let sut: FetchAllEquipmentsUseCase;
 
 describe('Find All Equipments Use Case', () => {
   beforeEach(() => {
     equipmentsRepository = new InMemoryEquipmentRepository();
     departmentsRepository = new InMemoryDepartmentRepository();
-    sut = new FindAllEquipmentsUseCase(equipmentsRepository);
+    sut = new FetchAllEquipmentsUseCase(equipmentsRepository);
   });
 
   it('should be able list all Equipments', async () => {
-    await departmentsRepository.departments.push({
-      name: 'IOT',
-      cost_center: 2420424,
-      is_board: false,
-      board: 'Tecnologia da Informação',
+    await equipmentsRepository.equipments.push(
+      makeCreateEquipment(),
+      makeCreateEquipment(),
+      makeCreateEquipment(),
+      makeCreateEquipment(),
+      makeCreateEquipment(),
+      makeCreateEquipment(),
+    );
+
+    const { equipments, totalCount } = await sut.execute({
+      params: {},
     });
 
-    await equipmentsRepository.equipments.push({
-      id: '01-005-00132',
-      brand: 'Dell',
-      model: 'XPTO',
-      department: 'TI',
-      status: 'activate',
-      ram: '16GB',
-      supplier: '',
-      invoice: '',
-      warranty: '',
-      purchase_date: '',
-      cpu: '',
-      slots: 0,
-      storage0_type: '',
-      storage0_syze: 0,
-      storage1_type: '',
-      storage1_syze: 0,
-      video: '',
-      service_tag: '',
-    });
-
-    await equipmentsRepository.equipments.push({
-      id: '01-005-00134',
-      brand: 'Dell',
-      model: 'XPTO',
-      department: 'TI',
-      status: 'activate',
-      ram: '16GB',
-      supplier: '',
-      invoice: '',
-      warranty: '',
-      purchase_date: '',
-      cpu: '',
-      slots: 0,
-      storage0_type: '',
-      storage0_syze: 0,
-      storage1_type: '',
-      storage1_syze: 0,
-      video: '',
-      service_tag: '',
-    });
-
-    await expect(() => {
-      sut.execute();
-    }).resolves;
+    expect(equipments[0]).toBeInstanceOf(Equipment);
+    expect(totalCount).toEqual(6);
   });
 });
--- src/domain/inventory/use-cases/equipment/fetch-all-equipments.ts
+++ src/domain/inventory/use-cases/equipment/fetch-all-equipments.ts
@@ -1,12 +1,22 @@
 import { PaginationParams } from '@/core/repositories/pagination-params';
 import { IEquipmentRepository } from '@/domain/inventory/repository/equipment.repository';
+import { Equipment } from '../../entity/equipment';
+
+type FetchAllEquipmentsRequest = {
+  params: PaginationParams;
+};
+
+type FetchAllEquipmentsResponse = {
+  totalCount?: number;
+  equipments: Equipment[];
+};
 
 export class FetchAllEquipmentsUseCase {
   constructor(private equipmentRepository: IEquipmentRepository) {}
 
   async execute({
     params,
-  }: FetchAllEquipmentsInput): Promise<FetchAllEquipmentsOutput> {
+  }: FetchAllEquipmentsRequest): Promise<FetchAllEquipmentsResponse> {
     const { equipments, totalCount } = await this.equipmentRepository.findMany({
       ...params,
     });
@@ -21,45 +31,3 @@ export class FetchAllEquipmentsUseCase {
     };
   }
 }
-
-type FetchAllEquipmentsInput = {
-  params: PaginationParams;
-};
-
-type FetchAllEquipmentsOutput = {
-  totalCount?: number;
-  equipments: {
-    props: {
-      id: string;
-      status: string;
-      currentUser: string | null;
-      patrimony: string | null;
-      type: string | null;
-      brand: string | null;
-      model: string | null;
-      serviceTag: string | null;
-      purchase: {
-        invoice: string | null;
-        supplier: string | null;
-        purchaseDate: Date | null;
-        warranty: string | null;
-      };
-      department: {
-        id: number | null;
-        name: string | null;
-      };
-      config: {
-        cpu: string | null;
-        ram: string | null;
-        video: string | null;
-        storage: {
-          slots: number | null;
-          storage0Type: string | null;
-          storage0Syze: number | null;
-          storage1Type: string | null;
-          storage1Syze: number | null;
-        };
-      };
-    };
-  }[];
-};
